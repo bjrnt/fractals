da771f4526cb45c8296f526fe8ec9468d5e662a1
Björn Tegelund <bjorn@tumblbug.com>
2017-02-11 00:21:16 +0900

    Add parrallelism when rendering images

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+extern crate rayon;
 extern crate image;
 extern crate num;
 
-use mandelbrot::Mandelbrot;
 mod mandelbrot;
 
+use mandelbrot::Mandelbrot;
 use std::env;
 use std::fs::{File, create_dir};
+use rayon::prelude::*;
 
 static OUTPUT_DIRECTORY: &'static str = "./output";
 static MAX_ITERATIONS: u32 = 255;
@@ -16,12 +18,13 @@ fn main() {
 
     let _ = create_dir(OUTPUT_DIRECTORY);
 
-    for i in 1..10 {
+    let mut iters: Vec<_> = (1..11).collect();
+    iters.par_iter_mut().enumerate().for_each(|(i, _)| {
         let path = format!("{}/mandel-{}.png", OUTPUT_DIRECTORY, i);
         let mut img_buf2 = image::ImageBuffer::new(side_length, side_length);
         let mandel = Mandelbrot::new(MAX_ITERATIONS);
         mandel.draw(&mut img_buf2);
-        let ref mut f_out = File::create(path).unwrap();
+        let f_out = &mut File::create(path).unwrap();
         let _ = image::ImageLuma8(img_buf2).save(f_out, image::PNG);
-    }
+    })
 }
